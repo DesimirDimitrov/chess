a0f527674a8d8a142e1dd4c0941441521e299777
Desimir Dimitrov <desko007@gmail.com>
2022-07-02 13:59:17 +0300

    Create basic register and login

--- packages/backend/start/routes.ts
+++ packages/backend/start/routes.ts
@@ -17,10 +17,35 @@
 | import './routes/customer'
 |
 */
-
+import Database from '@ioc:Adonis/Lucid/Database'
+import Hash from '@ioc:Adonis/Core/Hash'
 import Route from '@ioc:Adonis/Core/Route'
 import { testVariable } from '@desimir/chess-shared/dist'
 
 Route.get('/', async () => {
   return { hello: testVariable }
+})
+
+Route.post('api/register', async ({ request }) => {
+  const firstName = request.input('first_name')
+  const lastName = request.input('last_name')
+  const email = request.input('email')
+  const password = request.input('password')
+
+  // selecting table also returns an instance of the query builder
+  await Database.table('users')
+    .returning('id')
+    .insert({
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      password: await Hash.make(password),
+    })
+})
+
+Route.post('api/login', async ({ auth, request }) => {
+  const email = request.input('email')
+  const password = request.input('password')
+
+  return await auth.use('api').attempt(email, password)
 })
